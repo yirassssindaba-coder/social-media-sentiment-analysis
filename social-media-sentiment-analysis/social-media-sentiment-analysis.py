024ccaa1db79eefe47cf5785e80b0eff250eb2f2
sector2020world <ocaviantyoroberto@gmail.com>
2025-10-25 18:29:46 +0700

    chore: remove duplicate files in social-media-sentiment-analysis

--- social-media-sentiment-analysis/social-media-sentiment-analysis.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import nltk
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
-
-print("pandas", pd.__version__)
-nltk.download("vader_lexicon", quiet=True)
-sid = SentimentIntensityAnalyzer()
-print(sid.polarity_scores("I love this product"))
